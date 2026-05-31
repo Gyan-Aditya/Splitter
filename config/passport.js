980622dcc8beb6da2a7c52d55f5eb512110c333c
Scorpion <btejanshu@gmail.com>
2026-05-31 10:18:22 +0530

    Deployement redirect of Oauth

--- config/passport.js
+++ config/passport.js
@@ -41,7 +41,7 @@ export default function passportConfigurations() {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://splitter-weog.onrender.com/auth/google/callback"
   },
     async (accessToken, refreshToken, profile, done) => {
       try {
